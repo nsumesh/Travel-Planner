7015440312c62fd5b1b78df079d3229fa6da5054
Ishan Bhardwaj <ibhardwaj@umass.edu>
2023-03-31 22:12:09 -0400

    Added basic full-stack functionality

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const DataInterface = require('./data-interface.js');
+const DataInterface = require('./backend/data-interface.js');
 
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Prefs!');
+	// res.send('Prefs!');
+	res.sendFile(__dirname + '/frontend/index.html');
 })
 
 app.get('/cards', (req, res) => {
@@ -19,8 +20,8 @@ app.get('/lodging', (req, res) => {
 	res.send('Lodging!');
 })
 
-app.get('/transport', (req, res) => {
-	res.send('Transport!');
+app.get('/internal-transport', (req, res) => {
+	res.send('Internal Transport!');
 })
 
 app.get('/summary', (req, res) => {
