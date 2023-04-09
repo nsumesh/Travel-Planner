efca9298f03b2f2109af0cb8d9f6f4f3b7fde3cb
Ishan Bhardwaj <ibhardwaj@umass.edu>
2023-04-09 01:14:30 -0400

    Added budget and other filters

--- frontend/homepage-static/grabber.js
+++ frontend/homepage-static/grabber.js
@@ -4,13 +4,21 @@ function grabPreferences()
   let end = document.getElementById("end-place").value;
   let departDate = document.getElementById("depart-date").value;
   let adultCount = document.getElementById("adult-count").value;
+  let maxAmount = document.getElementById("budget").value;
   
   let package = { 
     originLocationCode: start,
     destinationLocationCode: end,
     departureDate: departDate,
-    adults: adultCount
+    adults: adultCount,
+    currencyCode: 'USD',
+    max: 25 // cap on number of results sent back
   };
+
+  if(maxAmount != '')
+  {
+    package["maxPrice"] = maxAmount;
+  }
 
   fetch('/initial-preferences', {
     method: 'POST',
