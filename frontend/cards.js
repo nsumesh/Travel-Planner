f07378624470b03819f50fac97f58b5d5cbf0f44
Hekera <40907168+Hekera@users.noreply.github.com>
2023-04-09 22:13:24 -0400

    cards page

--- frontend/cards.js
+++ frontend/cards.js
@@ -1,26 +1,48 @@
-const cards = ["flight"]; //TODO add others
+const cards = {//TODO add others
+	transportation: [
+		{key: "iata", factory: (data) => createImageElement(`https://daisycon.io/images/airline/?iata=${data}`)},
+		{key: "name", factory: (data) => createGenericElement(data, "h3")},
+		{key: "info", factory: (data) => createGenericElement(data, "div")}
+	]
+};
 
-loadCardData();
+loadCards();
 
 function capitalizeFirstLetter(string) {
+	
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function loadCardData() {
+function createGenericElement(data, tag) {
 	
-	for (const card of cards) {
-		let data = localStorage.getItem(field);
+	let element = document.createElement(tag);
+	element.innerHTML = data;
+	return element;
+}
+
+function createImageElement(data) {
+	
+	let icon = document.createElement("img");
+	icon.src = data;
+	return icon;
+}
+
+function loadCards() {
+	
+	for (const [card, components] of Object.entries(cards)) {
 		let id = `#${card}-card `;
-		let info = document.querySelector(id + ".card-info");
+		let details = document.querySelector(id + ".card-details");
 		let button = document.querySelector(id + ".submit-button");
-		if (!info || !button) {
+		if (!details || !button) {
 			continue;
 		}
-		if (data) {
-			button.innerHTML = "Change " + capitalizeFirstLetter(card); //TODO
-			
+		if (components.every(comp => localStorage[card + "_" + comp.key])) {
+			for (const component of components) {
+				details.appendChild(component.factory(localStorage[card + "_" + component.key]));
+			}
+			button.innerHTML = "Change " + capitalizeFirstLetter(card);
 		} else {
-			
+			details.appendChild(createImageElement(`./${card}-icon.png`));
 		}
 	}
 }
